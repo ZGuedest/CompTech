d675fd6364babc142851a83ff7f1cfc9246d08ea
Zguedest <zucelys.zge@gmail.com>
2023-02-23 18:40:05 +0100

    formulario cargar producto

--- addProduct.js
+++ addProduct.js
@@ -18,37 +18,36 @@ function addProducto(arrayPr){
     debugger
     let main = document.getElementsByTagName("main")[0]
     main.innerHTML = `
-        <div id= "addPro" class="container mt-5 mb-5 col-4">
-            <div class="abs-center">
-                <form action="#" class="mw-40 border p-3 form">
-                    <div class="form-group">
-                        <label for="">${arrayPr[arrayPr.length-1][0]}</label>
-                        <input type="name" class="form-control" id="" placeholder=${arrayPr[arrayPr.length-1][1]}>
-                    </div>
-                    <div class="form-group">
-                        <label >${arrayPr[arrayPr.length-1][2]}</label>
-                        <input type="number" class="form-control" id="" placeholder=${arrayPr[arrayPr.length-1][3]}>
-                    </div>
+    <div id= "addPro" class="container mt-5 mb-5 col-4">
+        <div class="abs-center">
+            <form action="http://localhost:5000/api/v1/products" method="post" enctype="multipart/form-data" class="mw-40 border p-3 form">
+                <div class="form-group">
+                    <label for="name">${arrayPr[arrayPr.length-1][0]}</label>
+                    <input type="name" name="name" class="form-control" id="name" placeholder=${arrayPr[arrayPr.length-1][1]}>
+                </div>
+                <div class="form-group">
+                    <label  for="price">${arrayPr[arrayPr.length-1][2]}</label>
+                    <input type="number" name="price" class="form-control" id="price" placeholder="${arrayPr[arrayPr.length-1][3]}">
+                </div>
 
-                    <div class="form-group">
-                        <label for="">${arrayPr[arrayPr.length-1][4]}</label>
-                        <input type="name" class="form-control" id="" placeholder=${arrayPr[arrayPr.length-1][5]}>
-                    </div>
-                
-                    <div class="form-group">
-                        <label for="">${arrayPr[arrayPr.length-1][6]}</label>
-                        <textarea class="form-control" id="" rows="3"></textarea>
-                    </div>
-                    <div>
-                        <label class="form-label" for="customFile">${arrayPr[arrayPr.length-1][7]}</label>
-                        <input type="file" class="form-control" id="customFile" />
-                    </div>
-                    <div class="form-group">
-                        <button type="button" class="btn mt-3 btn-dark">${arrayPr[arrayPr.length-1][8]}</button>
-                    </div>
-                </form>
-            </div>
-        </div>`
-    
+                <div class="form-group">
+                    <label for="email">${arrayPr[arrayPr.length-1][4]}</label>
+                    <input  for="email" name="email" type="email"  class="form-control" id="email" placeholder=${arrayPr[arrayPr.length-1][5]}>
+                </div>
+            
+                <div class="form-group">
+                    <label for="description">${arrayPr[arrayPr.length-1][6]}</label>
+                    <textarea class="form-control" id="description" name="description" rows="3"></textarea>
+                </div>
+                <div>
+                    <label class="form-label" for="photo">${arrayPr[arrayPr.length-1][7]}</label>
+                    <input type="file" class="form-control" id="photo" name="photo">
+                </div>
+                <div class="form-group">
+                    <button type="submit" class="btn mt-3 btn-dark">${arrayPr[arrayPr.length-1][8]}</button>
+                </div>
+            </form>
+        </div>
+    </div>`
     
 }
